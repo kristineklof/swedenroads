1fc88670d60c6d9b23b06c5e1a3fb4d75b3cdacd
Kristin Eklöf <kristin@salbo.ai>
2020-10-28 22:24:51 +0100

    Remove section length selection

--- ImportNVDB_Data.r
+++ ImportNVDB_Data.r
@@ -6,6 +6,7 @@
 ##############################################
 ## Import NVDB Shapefile
 nvdb_homo <- st_read("C:/Users/winte/Swedenroads_homo_v2/Swedenroads_homo_v2_Sverige_shape/Swedenroads_homo_v2_Sverige.shp")
+
 head(nvdb_homo)
 nrow(nvdb_homo)
 str(nvdb_homo)
@@ -62,17 +63,4 @@ nrow(nvdb_norrbotten)
 sum(nvdb_norrbotten$Shape_Leng)/1000
 st_write(nvdb_norrbotten, "C:/Users/winte/Swedenroads_outputs/nvdb_norrbotten_201018.shp", driver="ESRI Shapefile") 
 
-##############################################
-## Extract sections longer than 50 m
-nvdb_nat <- nvdb_nat[Shape_Leng >= 50]
 
-itShouldSummarizePartialNetwork<- function(nvdbnat){
-    stopifnot(round(sum(nvdb_nat$Shape_Leng/1000),2) == 83840.95)
-    stopifnot(nrow(nvdb_nat) == 376025)
-    stopifnot(round(mean(nvdb_nat$Shape_Leng),1) == 223)
-    stopifnot(round(max(nvdb_nat$Shape_Leng),1) == 10836.9)
-    stopifnot(round(min(nvdb_nat$Shape_Leng),1) == 50)
-    stopifnot(round(sd(nvdb_nat$Shape_Leng),1) == 231.5)
-    print("OK") 
-}
-itShouldSummarizePartialNetwork(nvdb_nat)
