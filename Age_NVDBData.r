b435500d22869bee0b14110071a3d614418de9e4
Kristin Eklöf <kristin@salbo.ai>
2020-11-17 08:46:08 +0100

    Change age to handle date format

--- Age_NVDBData.r
+++ Age_NVDBData.r
@@ -2,24 +2,34 @@
 #            Create Age variable for NVDB data
 #=================================================================#
 
+IsDate <- function(x) inherits(x, "Date")
+
 CalculateAge <- function(TreatmentDate){
-    TreatmentDate <- as.character(TreatmentDate)
-    Age <- 2020-as.numeric(substring(TreatmentDate, 1, 4))
-    return(Age)
+    #print(IsDate(TreatmentDate))
+    if(!IsDate(TreatmentDate)){
+        TreatmentDate <- as.Date(as.character(TreatmentDate), format="%Y%m%d")
+    }
+
+    #TreatmentDate <- as.Date(TreatmentDate, origin = "1970-01-01")
+    Age <- year(as.Date("2020-01-01"))-year(TreatmentDate)
+    return(as.integer(Age))
 }
 
 itShouldCalculateAge <- function(){
-    TreatmentDate <- c(19850912, 19881110, 20190506)
-    res <- CalculateAge(TreatmentDate)
-    gold <- c(35,32,1)
+    TreatmentDate <- c(19850912, 19881110, 20190506, 20151201)
+    TreatmentDateD <- as.Date(as.character(TreatmentDate), format="%Y%m%d")
+    res <- CalculateAge(TreatmentDateD)
+    #print(res)
+    gold <- c(35,32,1, 5)
     stopifnot(res == gold)
+    stopifnot(is.integer(res) == TRUE)
 }
 itShouldCalculateAge()
 
 AddIfAgeMissing <- function(dat){
     # Impute missing age
     dat <- dat %>% group_by(Municipality,tkl8,RoadCategory,RoadType) %>% 
-              mutate(Age_imp = round(mean(Age, na.rm = TRUE), digits=0)) %>%
+              mutate(Age_imp = as.integer(round(mean(Age, na.rm = TRUE), digits=0))) %>%
               mutate(Age = coalesce(Age,Age_imp)) %>%
               select(-Age_imp) %>%
               ungroup()
