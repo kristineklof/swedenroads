1effc9745d8dc4d3cb47c7c91137ffd68e8d6c96
Kristin Eklöf <kristin@salbo.ai>
2020-11-17 08:50:24 +0100

    Add coverage and different survival models

--- QClassFunction.r
+++ QClassFunction.r
@@ -44,24 +44,78 @@ itShouldSetClasses <- function(){
 
 itShouldSetClasses()
 
-CreateServiceLifeData <- function(dat, mod, percentil = 0.75){
+CreateServiceLifeData <- function(dat, survdat, metod = "AFT", distribution, percentil_high, percentil_low, div = "yes", divclass = 5){
   setDT(dat)
   myVector <- c("tkl8", "AADT", "AADT_heavy", "PavementType", "Region", 
                 "BearingCapacityClass", "RoadType", "RoadWidth", "SpeedLimit")
   preddat <- dat[, ..myVector]
 
-  # Predict Weibull lifetimes
-  dat[, PredictedServiceLife := round(predict(mod, preddat, type="quantile", p=percentil), digits=0)]
-  dat[, RemainingServiceLife := PredictedServiceLife-Age]
+  if(metod == "Cox"){
+      cox_reg <- CoxPH(survdat)
+
+      forecast <- coxed(cox_reg, newdata=preddat, method="npsf")
+       # Predict Cox lifetimes
+      dat[, PredictedServiceLife := round(forecast$exp.dur, digits=0)]
+      dat[, RemainingServiceLife := PredictedServiceLife-Age]
+  } else {
+    reg_d_uh <- AFT_Regression(survdat,distribution)
+
+    dat[, Pred_50perc := round(predict(reg_d_uh, preddat, type="quantile", p=percentil_high), digits=0)]
+    dat[, Pred_75perc := round(predict(reg_d_uh, preddat, type="quantile", p=percentil_low), digits=0)]
+
+    # Predict  lifetimes
+    if(div == "yes"){
+        dat[, PredictedServiceLife := ifelse(tkl8 <= divclass, Pred_75perc, Pred_50perc)]
+        dat[, RemainingServiceLife := PredictedServiceLife-Age]
+    } else if(div == "median") {
+        dat[, PredictedServiceLife := Pred_50perc]
+        dat[, RemainingServiceLife := PredictedServiceLife-Age]
+    } else {
+        dat[, PredictedServiceLife := Pred_75perc]
+        dat[, RemainingServiceLife := PredictedServiceLife-Age]      
+    }
+  }
 
   # If PavementType == "Försegling" subtract 8 years to remaining service life, 
   # "Indränkt makadam", "Tunnskikt", or "Övrigt", subtract 5 years to remaining service life, 
-  dat[, RemainingServiceLife:= ifelse(PavementType == "Försegling", RemainingServiceLife-10, RemainingServiceLife)]
-  dat[, RemainingServiceLife := ifelse(PavementType == "Indränkt makadam" | PavementType == "Tunnskikt" | PavementType == "Övrigt", RemainingServiceLife-5, RemainingServiceLife)]
+  dat <- CoverageServiceLifes(dat)
 
   # Set classes
   dat <- SetClasses(dat)
   
   return(setDT(dat))
 }
+
+CoverageServiceLifes <- function(dat){
+  setDT(dat)
+ 
+  dat[, RemainingServiceLife := ifelse(Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning"), round(RemainingServiceLife*0.3,digits=0), RemainingServiceLife)]
+  #dat[, RemainingServiceLife:= ifelse(PavementType == "Försegling" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 5, RemainingServiceLife)]
+  #dat[, RemainingServiceLife := ifelse(PavementType == "Indränkt makadam" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 5, RemainingServiceLife)]
+  #dat[, RemainingServiceLife := ifelse(PavementType == "Övrigt" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 5, RemainingServiceLife)]
+  #dat[, RemainingServiceLife := ifelse(PavementType == "Tunnskikt" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 7, RemainingServiceLife)]
+  #dat[, RemainingServiceLife := ifelse(PavementType == "Ytbehandling på bituminöst underlag" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 7, RemainingServiceLife)]
+  #dat[, RemainingServiceLife := ifelse(PavementType == "Ytbehandling på grus" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 7, RemainingServiceLife)]
+  #dat[, RemainingServiceLife := ifelse(PavementType == "Halvvarm" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 7, RemainingServiceLife)]
+  #dat[, RemainingServiceLife := ifelse(PavementType == "Varm" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 7, RemainingServiceLife)]
+  #dat[, RemainingServiceLife := ifelse(PavementType == "Varm stenrik" & (Coverage %in% c("Fläckvis", "Spårlagning", "Fläckvis <20%", "Fläckvis >20%", "Kanthäng", "Fläckvis spårlagning")), 7, RemainingServiceLife)]
+
+  return(dat)
+}
+
+itShouldSetServiceLivesForFläckvisCoverage <- function(){
+  testdat <- data.frame(PavementType = c("Försegling","Försegling","Försegling","Tunnskikt","Tunnskikt"),
+                        Coverage = c("Fläckvis",NA,"Heltäckande","Spårlagning","Fläckvis >20%"),
+                        tkl8 = c(1,1,1,1,7),
+                        RemainingServiceLife = c(22,10,10,10,10))
+  
+  res <- CoverageServiceLifes(testdat)
+  #print(res)
+  gold <- c(7,10,10,3,3)
+  stopifnot(res$RemainingServiceLife == gold)
+}
+
+itShouldSetServiceLivesForFläckvisCoverage()
+
+
 
