d975193621ba3546f48dfcdbd1fb4e34277b9281
Kristin Eklöf <kristin@salbo.ai>
2020-12-04 11:06:01 +0100

    Add descriptive stats

--- DescriptiveStatistics.r
+++ DescriptiveStatistics.r
@@ -3,15 +3,27 @@
 #=================================================================#
 
 swedt_PCI <- st_read( "C:/Users/winte/Swedenroads_outputs/sweden_v3_pci201119.shp") 
-
 head(swedt_PCI)
-setDT(swedt_PCI)
-swedt_PCI <- PCIClass(setDT(swedt_PCI))
 
+setDT(swedt_PCI)
+swedt_PCI <- PCIClass(swedt_PCI)
+swedt_PCI[, PCIClass := as.factor(PCIClass)]
+str(swedt_PCI)
+
+# Quantative descriptive statistics for report
 DescriptiveStats(swedt_PCI$Length)
 DescriptiveStats(swedt_PCI$Age)
 DescriptiveStats(swedt_PCI$AADT_hv)
 DescriptiveStats(swedt_PCI$AADT)
+
+# Qualitative descriptive statistics for report
+QualitativeStats(swedt_PCI, quo(tkl8), quo(Length))
+QualitativeStats(swedt_PCI, quo(PvmntTy), quo(Length))
+QualitativeStats(swedt_PCI, quo(DoU2017), quo(Length))
+QualitativeStats(swedt_PCI, quo(RoadTyp), quo(Length))
+QualitativeStats(swedt_PCI, quo(RdCtgry), quo(Length))
+
+
            
 DescriptiveStats <- function(var){
     dstats <- data.frame(Mean = round(mean(var, na.rm=TRUE), digits = 0),
@@ -25,18 +37,6 @@ DescriptiveStats <- function(var){
     return(dstats)
 }
 
-QualitativeStats(swedt_PCI, quo(tkl8), quo(Length))
-QualitativeStats(swedt_PCI, quo(PvmntTy), quo(Length))
-QualitativeStats(swedt_PCI, quo(DoU2017), quo(Length))
-
-maintstandlengthdou <- swedt_PCI %>%
-              group_by(DoU2017) %>%
-              summarise(grouplen = sum(Length)/1000,
-                        percabove= sum(Length[(rt_m17_ > SP_mant & RodWdth > 6) | (rt_m15_ > SP_mant & RodWdth <= 6)| IRI_r_p > IRI_mnt], na.rm = TRUE)/1000/grouplen,
-                        lenabove= percabove*grouplen)
-print(maintstandlengthdou)
-sum(maintstandlengthdou$lenabove)/sum(maintstandlengthdou$grouplen)
-
 QualitativeStats <- function(df, grp.var, uniq.var){
     classlengtht <- df %>%
               group_by(!!grp.var) %>%
@@ -46,15 +46,48 @@ QualitativeStats <- function(df, grp.var, uniq.var){
     return(classlengtht)
 }
 
+#################################################################
+# PCI statistics for report
+maintstandlengthdou <- swedt_PCI %>%
+              group_by(DoU2017) %>%
+              summarise(grouplen = sum(Length)/1000,
+                        percabove= sum(Length[(rt_m17_ > SP_mant & RodWdth > 6) | (rt_m15_ > SP_mant & RodWdth <= 6)| IRI_r_p > IRI_mnt], na.rm = TRUE)/1000/grouplen,
+                        lenabove= percabove*grouplen)
+print(maintstandlengthdou)
+sum(maintstandlengthdou$lenabove)/sum(maintstandlengthdou$grouplen)
+
 quantile(swedt_PCI$PCI, probs = c(0.05, 0.25, 0.5, 0.75, 0.95), na.rm=TRUE) # quartile
 
 DescriptiveStats(swedt_PCI$PCI)
 
 pclasslength <- swedt_PCI %>%
-              group_by(PCIClss) %>%
+              group_by(PCIClass) %>%
               summarise(grouplen = sum(Length)/1000) %>%
               mutate(prop = grouplen/sum(grouplen))
 print(pclasslength)
+
+# PCI barchart for report
+fill <- c("forestgreen", "chartreuse3","gold","orangered3","darkred")
+
+cond_p <- swedt_PCI %>%
+  mutate(Region = recode(Region, Ost="Öst", Vast="Väst")) %>%
+  mutate(PCIClass = factor(PCIClass, levels = c("5","4","3","2","1"))) %>%
+  mutate(PCIClass = recode(PCIClass, "5" ="Mycket bra", "4" = "Bra", "3" = "Tillfredsställande", "2" = "Dålig", "1" = "Mycket dålig")) %>%
+  group_by(Region, PCIClass) %>%
+  summarise(grouplen = sum(Length)/1000) %>%
+  mutate(percentage = grouplen/sum(grouplen)) %>%
+  ggplot(aes(x = Region, y = percentage, fill = PCIClass, label = paste0(round(100*percentage,digits=0)," %"))) +
+    geom_bar(position = 'fill', stat = 'identity') +
+    scale_fill_manual(values=fill) +
+    labs(y="", x = "") +
+    scale_y_continuous(labels = scales::percent) +
+    theme(legend.position="right", legend.direction="vertical",
+                   legend.title = element_blank(), 
+                   legend.text=element_text(size=16), 
+                   axis.text=element_text(size=16)) +
+    geom_text(size = 3, position = position_stack(vjust = 0.5))
+
+print(cond_p)
 
 #####################################################
 # Plot index curve
@@ -65,9 +98,10 @@ PlotIndexCurve <- function(cutoff, actual){
 
     p <- ggplot(data = df, mapping = aes(x = x)) + 
                 stat_function(fun = index, size = 1) +
-                geom_hline(yintercept=20, linetype="dashed", color = "red") +
-                theme(axis.text.x = element_text(face="bold", size=12),
-                     axis.text.y = element_text(face="bold", size=12)) +
+                geom_hline(yintercept=20, linetype="dashed", color = "red", size = 2) +
+                theme(axis.text.x = element_text(size=16),
+                      axis.text.y = element_text(size=16),
+                      axis.title = element_text(size=14)) +
                 scale_x_continuous(name="(IRI underhållsstandard - IRI mätvärde)/IRI underhållsstandard", limits=c(0, 5)) +
                 scale_y_continuous(name="Indexvärde", breaks=seq(0,100,20))
 
@@ -93,8 +127,8 @@ swebel <- swedt_PCI %>%
 print(swebel, n=Inf)
 sum(swebel$grouplen)/(sum(swedt_PCI$Length)/1000)
 sum(swedt_PCI[is.na(Tretmnt)]$Length)/sum(swedt_PCI$Length)
-# Beläggningsåtgärder per region
 
+# Beläggningsåtgärder per region
 regbel <- swedt_PCI %>%
               filter(!is.na(Tretmnt)) %>%
               group_by(Region,Tretmnt) %>%
@@ -106,6 +140,54 @@ print(regbel, n=Inf)
 
 write_xlsx(regbel,"C:/Users/winte/OneDrive - Salbo Konsult AB/salbo.ai/Transportföretagen/Data/regbel.xlsx")
 
+# Klassificerad
+swebel_klass <- swedt_PCI %>%
+              filter(!is.na(PvmntTy)) %>%
+              group_by(PvmntTy) %>%
+              summarise(grouplen = sum(Length)/1000) %>%
+              mutate(prop = grouplen/sum(grouplen)) %>%
+              top_n(n=10) %>%
+              arrange(desc(prop))
+print(swebel_klass, n=Inf)
+
+# Klassificerad
+klass_manf <- swedt_PCI %>%
+              filter(!is.na(PvngMth)) %>%
+              group_by(PvmntTy, PvngMth) %>%
+              summarise(grouplen = sum(Length)/1000) %>%
+              mutate(prop = grouplen/sum(grouplen)) %>%
+              top_n(n=10) %>%
+              arrange(PvngMth, desc(prop))
+print(klass_manf, n=Inf)
+
+# Klass + trafikmängd
+swebel_klass_tkl <- swedt_PCI %>%
+              filter(!is.na(Tretmnt)) %>%
+              group_by(tkl8, Tretmnt) %>%
+              summarise(grouplen = sum(Length)/1000) %>%
+              mutate(prop = grouplen/sum(grouplen)) %>%
+              top_n(n=5) %>%
+              arrange(tkl8, desc(prop))
+print(swebel_klass_tkl, n=Inf)
+
+# Klassificerad historisk (PMS)
+head(lans_dt)
+
+historisk_bel <- lans_dt %>%
+              filter(!is.na(Atgard1)) %>%
+              group_by(Atgard1) %>%
+              summarise(grouplen = sum(langd)/1000) %>%
+              mutate(prop = grouplen/sum(grouplen)) %>%
+              #top_n(n=10) %>%
+              arrange(desc(prop))
+print(historisk_bel, n=Inf)
+
+# Join with gruppering
+swebel_kat <- left_join(swebel, historisk_bel[,1:2], by = c("Tretmnt" = "Beltyp"))
+swebel_kat$Atgard2 <- substring(swebel_kat$Atgard2,5)
+print(swebel_kat, n=Inf)
+
+write_xlsx(swebel_kat,"C:/Users/winte/OneDrive - Salbo Konsult AB/salbo.ai/Transportföretagen/Data/beläggningsåtgärder_grupperad.xlsx")
 
 ##############################################################
 # Sammanställning missing data
@@ -141,6 +223,8 @@ sum(outdat_swe$Längd[is.na(outdat_swe$IRI)])/1000
 
 sum(outdat_swe$Längd[is.na(outdat_swe$Spårdjup)])/sum(outdat_swe$Längd)
 sum(outdat_swe$Längd[is.na(outdat_swe$Spårdjup)])/1000
+
+
 
 
 
