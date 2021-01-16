8b20b0c871ff515d43ff767a992f85c0a17c97a2
Kristin Eklöf <kristin@salbo.ai>
2021-01-16 15:26:01 +0100

    Add shapefiles for scenarios current and minimize

--- Create2030shapefile.r
+++ Create2030shapefile.r
@@ -2,27 +2,36 @@
 #                 Import and create output for 2030
 #=================================================================#
 
-pci2030 <- read.xlsx("C:/Users/winte/Swedenroads_outputs/2030_PCI_20201218.xlsx")
-head(pci2030)
-names(pci2030) <- c("Objectd","Year","PCI")
+pci2030_out <- fread("C:/Users/winte/Swedenroads_outputs/7018-Scenario6.csv")
+pci2030_out <- fread("C:/Users/winte/Swedenroads_outputs/7046-Scenario9b.csv")
+pci2030_out <- fread("C:/Users/winte/Swedenroads_outputs/7045-Scenario4e.csv")
 
-setDT(pci2030)
-pci2030 <- PCIClass(pci2030)
+head(pci2030_out)
+names(pci2030_out) <- c("Objectd","Year","PCI")
+
+setDT(pci2030_out)
+pci2030_out <- PCIClass(pci2030_out)
+pci2030_out <- pci2030_out[Year == 2030]
 
 swedt_PCI <- st_read( "C:/Users/winte/Swedenroads_outputs/sweden_v3_pci201206.shp") 
 setDT(swedt_PCI)
 head(swedt_PCI)
 cols <- c("Objectd","Length","geometry")
 
-pci2030 <- pci2030[swedt_PCI[, ..cols], on = 'Objectd']
+pci2030_out <- pci2030_out[swedt_PCI[, ..cols], on = 'Objectd']
 
-pclasslength <- pci2030 %>%
+pclasslength <- pci2030_out %>%
               group_by(PCIClass) %>%
               summarise(grouplen = sum(Length)/1000) %>%
               mutate(prop = grouplen/sum(grouplen))
 print(pclasslength)
 
-quantile(pci2030$PCI, probs = c(0.05, 0.25, 0.5, 0.75, 0.95), na.rm=TRUE) # quartile
-mean(pci2030$PCI)
+quantile(pci2030_out$PCI, probs = c(0.05, 0.25, 0.5, 0.75, 0.95), na.rm=TRUE) # quartile
+mean(pci2030_out$PCI)
+nrow(pci2030_out)
 
-st_write(pci2030, "C:/Users/winte/Swedenroads_outputs/swedenroads_2030_v5.shp", driver="ESRI Shapefile", append=FALSE) 
+st_write(pci2030_out, "C:/Users/winte/Swedenroads_outputs/swedenroads_2030_v6.shp", driver="ESRI Shapefile", append=FALSE) 
+
+st_write(pci2030_out, "C:/Users/winte/Swedenroads_outputs/swedenroads_maintain_cur_2030_v1.shp", driver="ESRI Shapefile", append=FALSE) 
+
+st_write(pci2030_out, "C:/Users/winte/Swedenroads_outputs/swedenroads_minimize_def_2030_v2.shp", driver="ESRI Shapefile", append=FALSE) 
