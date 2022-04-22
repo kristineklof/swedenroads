f6a1a864af1123104f2ebaef15f49cc121ff8c22
Kristin Eklöf <kristin@salbo.ai>
2022-04-22 10:32:39 +0200

    Add shapefile outputs

--- Update 2022/Output_shapefiles.R
+++ Update 2022/Output_shapefiles.R
@@ -92,6 +92,8 @@ itShouldCheckIfAnyVariableHasNAs <- function(){
   stopifnot(sum(is.na(sweden_2022_dot$RoadTyp)) == 0)
   stopifnot(sum(is.na(sweden_2022_dot$RodWdth)) == 0)
   stopifnot(sum(is.na(sweden_2022_dot$Length)) == 0)
+  stopifnot(max(sweden_2022_dot$PCI) == 100)
+  stopifnot(min(sweden_2022_dot$PCI) == 0)
 }
 
 itShouldCheckIfAnyVariableHasNAs()
@@ -100,5 +102,68 @@ QualitativeStatsSingleGroup(sweden_2022_dot, quo(PCICls), quo(Length))
 
 # Join with geometry
 sweden_2022_dot <- dplyr::left_join(sweden_2022_dot,sw22_geo, by=c("Objectd" = "ID"))
+st_write(sweden_2022_dot, paste0(datapath,"2022/DOT/sweden_2022_dot_20220311.shp"))
 
-st_write(sweden_2022_dot, paste0(datapath,"2022/DOT/sweden_2022_dot_20220301.shp"))
+##########################################################
+# Output till Anders/swedenroads/våravägar
+swroads_cols <- c("id", "brghtsk", "hastght", "dou2017",
+                  "Ådt_frd", "Ådt_tng", "Ådt_mtr", "vägbrdd",
+                  "vägnmmr","vägktgr", "vägtyp", "längd",  
+                  "blggnngsd","PavementType", "blggnngst", "tackning",
+                  "spårdjp",  "iri","mätdatm","IRI_maint", "SP_maint",
+                   "län_nr", "kmmn_nr",
+                  "Ålder","PredictedServiceLife", "RemainingServiceLife",
+                  "PCI_22", "PCIClass_22")
+swedenroads_2022 <-  sw22[,..swroads_cols]
+swedenroads_2022 <- swedenroads_2022 %>%
+  dplyr::mutate(PavementType = ChangeBeltypNumerisk(PavementType)) %>%
+  dplyr::mutate(blggnngst = if_else(!is.na(PavementType), PavementType, blggnngst)) %>%
+  dplyr::mutate(tackning = ChangeTackningNumerisk(tackning))
+
+# Add classes
+pci2031_cur <- pci2030 %>% dplyr::filter(Year == 2031) %>%
+  dplyr::select(Objectd,PCIClass)
+names(pci2031_cur) <- c("id","IKls_1")
+
+pci2031_mcur <- pci2030_mcur %>% dplyr::filter(Year == 2031) %>%
+  dplyr::select(Objectd,PCIClass)
+names(pci2031_mcur) <- c("id","IKls_2")
+
+pci2031_min <- pci2030_min %>% dplyr::filter(Year == 2031) %>%
+  dplyr::select(Objectd,PCIClass)
+names(pci2031_min) <- c("id","IKls_3")
+
+swedenroads_2022 <- dplyr::left_join(swedenroads_2022, pci2031_cur, by = c("id"))
+swedenroads_2022 <- dplyr::left_join(swedenroads_2022, pci2031_mcur, by = c("id"))
+swedenroads_2022 <- dplyr::left_join(swedenroads_2022, pci2031_min, by = c("id"))
+
+swroads_cols2 <- c("id", "brghtsk", "hastght", "dou2017",
+                  "Ådt_frd", "Ådt_tng", "Ådt_mtr", "vägbrdd",
+                  "vägnmmr","vägktgr", "vägtyp", "längd",  
+                  "blggnngsd", "blggnngst", "tackning",
+                  "spårdjp",  "iri","mätdatm","IRI_maint", "SP_maint",
+                  "län_nr", "kmmn_nr",
+                  "Ålder","PredictedServiceLife", "RemainingServiceLife",
+                  "PCI_22", "PCIClass_22","IKls_1","IKls_2","IKls_3")
+swedenroads_2022 <-  swedenroads_2022[,..swroads_cols2]
+names(swedenroads_2022) <- c("id", "brghtsk", "hastght", "dou2017",
+                        "Ådt_frd", "Ådt_tng", "Ådt_mtr", "vägbrdd",
+                        "vägnmmr","vägktgr", "vägtyp", "längd",  
+                        "blggnngsd","blggnngst", "tackning",
+                        "spårdjp",  "iri","mätdatm","IRI_maint", "SP_maint",
+                        "län_nr", "kmmn_nr",
+                        "Ålder","FrvntdL", "ÅtrstnL",
+                        "Tllstnl", "IndxKls","IKls_1","IKls_2","IKls_3")
+# Join with geometry
+swedenroads_2022 <- dplyr::left_join(swedenroads_2022,sw22_geo, by=c("id" = "ID"))
+
+# Quality check
+head(swedenroads_2022)
+QualitativeStatsSingleGroup(swedenroads_2022, quo(IndxKls), quo(längd))
+QualitativeStatsSingleGroup(swedenroads_2022, quo(IKls_1), quo(längd))
+QualitativeStatsSingleGroup(swedenroads_2022, quo(IKls_2), quo(längd))
+QualitativeStatsSingleGroup(swedenroads_2022, quo(IKls_3), quo(längd))
+
+# Export
+st_write(swedenroads_2022, paste0(datapath,"2022/swedenroads_v1_2022.shp"))
+
